1057b55eb9bd0ecc033eaf0da7d3b1352e3d8a86
wangxin <2351625635@qq.com>
2020-04-08 17:21:27 +0800

    springboot2 jap

---
+++ spring-boot-04-web-jap/src/main/java/com/atwangxin/springboot/SpringBoot04WebJapApplication.java
@@ -0,0 +1,13 @@
+package com.atwangxin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot04WebJapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot04WebJapApplication.class, args);
+    }
+
+}
---
+++ spring-boot-04-web-jap/src/test/java/com/atwangxin/springboot/SpringBoot04WebJapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atwangxin.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot04WebJapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
